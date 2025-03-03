16a3c949f28685b16a434ce29dc6206ce6f4a93f
Sulistriawati Ramadhani <sulisulistria97@gmail.com>
2025-03-03 23:10:54 +0800

    first commit

---
+++ Tugas 1/Source Code/Asgdll.java
@@ -0,0 +1,12 @@
+public class Asgdll {
+  
+   public static void main(String[] args) {
+         
+        float f= 20.0f;
+        double fll;
+
+        /* Algoritma */
+	fll = 10.0f;
+	System.out.println ("f : "+f + "\nf11: "+fll);
+   }
+}
